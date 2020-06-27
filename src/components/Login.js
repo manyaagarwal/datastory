1d44f9c1f59b2b1d94e02253d75483d6ba619f25
agarwalmanya <manya18ag@gmail.com>
2020-06-28 00:59:19 +0530

    sign in and sign up

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,2 +1,80 @@
-import React from 'react'; 
+import React from 'react';
+import { Form, Input, Button, Checkbox,message } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { connect } from 'react-redux';
+import { signIn } from '../actions/user';
+import { Link } from 'react-router-dom';
 
+class LoginForm extends React.Component {
+    componentDidUpdate(prevProps){ 
+        if(prevProps.user !== this.props.user)
+        { 
+            console.log(this.props.user);
+            if(this.props.user.error !== null)
+            { 
+                message.error(this.props.user.error);
+            } 
+            if(this.props.user.user !== null)
+            { 
+                this.props.history.push('/');
+            } 
+        }
+    }
+    render() {
+        const onFinish = values => {
+            console.log('Received values of form: ', values);
+            this.props.signIn(values.username,values.password);
+        };
+
+        return (
+            <div className="loginContainer">
+                <img src="https://firebasestorage.googleapis.com/v0/b/datastory-a3e39.appspot.com/o/logo.png?alt=media&token=9b68ff2a-6028-42d8-b765-c286005ad369"
+                    width="400" />
+                <Form
+                    name="normal_login"
+                    className="login-form"
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                >
+                    <Form.Item
+                        name="username"
+                        rules={[{ required: true, message: 'Please input your Username!' }]}
+                    >
+                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your Password!' }]}
+                    >
+                        <Input
+                            prefix={<LockOutlined className="site-form-item-icon" />}
+                            type="password"
+                            placeholder="Password"
+                        />
+                    </Form.Item>
+                    <Form.Item>
+                        <Form.Item name="remember" valuePropName="checked" noStyle>
+                            <Checkbox>Remember me</Checkbox>
+                        </Form.Item>
+
+                        <a className="login-form-forgot" href="">
+                            Forgot password
+        </a>
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" className="login-form-button">
+                            Log in
+        </Button>
+        Or <Link to="/signup">register now!</Link>
+                    </Form.Item>
+                </Form>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+    user: state.user
+})
+
+export default connect(mapStateToProps, { signIn })(LoginForm);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,25 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { connect } from "react-redux";
+import { fetchUser } from "../actions/user"
 import React from "react";
 import App from "../App";
 import AppBar from "../components/AppBar";
 import Home from "../components/Home";
 import Country from "../components/Country";
+import Login from "../components/Login";
+import SignUp from "../components/SignUp"
 
 class Index extends React.Component { 
+    componentWillMount() {
+        this.props.fetchUser();
+      }
+
     render(){ 
         return(
             <BrowserRouter>
                 <Switch>
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/signup" component={SignUp} />
                     <AppBar>
                         <Route exact path='/' component={Home} />
                         <Route exact path='/country/:id' component={Country} />
@@ -21,4 +30,7 @@ class Index extends React.Component {
     }
 }
 
-export default Index; 
+export default connect(
+  null,
+  { fetchUser }
+)(Index);
