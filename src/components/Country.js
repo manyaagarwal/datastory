3c84fe7c7eec725567114bbf22065e47e27f308e
Cindy Pham <phamcindy619@utexas.edu>
2020-06-28 03:40:05 -0500

    Reverted Country.js

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,61 +1,11 @@
-import React from 'react';
-import { Table } from 'antd';
-import { FirebaseDB } from '../lib/firebase';
+import React from 'react'; 
 
-const columns = [
-    {
-        title: 'Year',
-        dataIndex: 'year',
-        key: 'year'
-    },
-    {
-        title: 'Average Annual Count',
-        dataIndex: 'avgCount',
-        key: 'avgCount'
-    }
-];
-
-function getData(countryId) {
-    let data = [];
-    FirebaseDB.collection(countryId.toString()).get()
-    .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            data.push({
-                key: doc.data().id, 
-                year: doc.data().year,
-                avgCount: doc.data().avgCount
-            });
-        });
-        return data;
-    });
-};
-
-class Country extends React.Component{
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            data: []
-        }
-    }
-
-    componentDidMount() {
+class Country extends React.Component{ 
+    render(){ 
         const {id} = this.props.match.params;
-        const fetched = getData(id);
-        this.setState({ data: fetched });
-        console.log(this.state.data); 
-    }
-
-    render() {
-        const {id} = this.props.match.params;
-
-        return (
-            <div className="container">
-                <h3>{id}</h3>
-                <Table columns={columns} dataSource={this.state.data} />
-            </div> 
-        );
+        return( 
+            <h1>{id}</h1>
+        )
     }
 }
 
