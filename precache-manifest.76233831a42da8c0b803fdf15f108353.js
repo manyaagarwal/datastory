4526a69b2511427b96746a516d16a8426f246fb5
agarwalmanya <manya18ag@gmail.com>
2020-06-28 18:13:56 +0530

    Updates

---
+++ precache-manifest.76233831a42da8c0b803fdf15f108353.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "419f5b7145095a4cdba812e6c28d8eb0",
+    "url": "/datastory/index.html"
+  },
+  {
+    "revision": "402b50d9a982950e59c4",
+    "url": "/datastory/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "be3ce9b71ba1d101a7be",
+    "url": "/datastory/static/css/main.dc559815.chunk.css"
+  },
+  {
+    "revision": "402b50d9a982950e59c4",
+    "url": "/datastory/static/js/2.a0a8b69d.chunk.js"
+  },
+  {
+    "revision": "544111c69119227bbf075a65771ccdb1",
+    "url": "/datastory/static/js/2.a0a8b69d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be3ce9b71ba1d101a7be",
+    "url": "/datastory/static/js/main.04c6f583.chunk.js"
+  },
+  {
+    "revision": "7fbf96df52df602c33a1",
+    "url": "/datastory/static/js/runtime-main.3c75267f.js"
+  }
+]);
